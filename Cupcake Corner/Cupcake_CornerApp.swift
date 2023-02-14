d6661310d8241ada5dac9e4bfe40d5fbab16ab40
bahadirersin <bahadirersin@gmail.com>
2023-02-14 11:17:50 +0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Bahadır Ersin on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
